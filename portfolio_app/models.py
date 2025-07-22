078549caeaf033e89e19a012d5a7288daf9dfbe2
aslamsujath29 <aslamsujath@thaagam.email>
2025-07-22 12:42:46 +0530

    migrations changes

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contact(models.Model):
     name=models.CharField(max_length=50,null=True,blank=True)
     email=models.EmailField(max_length=50,null=True,blank=True)
-    phone_number=models.IntegerField(max_length=20,null=True,blank=True)
+    phone_number=models.IntegerField(null=True,blank=True)
     remarks=models.TextField(null=True,blank=True)
-    services = models.CharField(null=True, blank=True)
+    services = models.CharField(max_length=50,null=True, blank=True)
     created_at = models.DateField(null=True, blank=True)
